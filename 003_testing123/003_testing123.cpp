a18af665ff96f788270eb0f5ede3d7179114eac8
shmkvdmd <demidka100@mail.ru>
2023-10-04 22:16:54 +0300

    !

---
+++ 003_testing123/003_testing123.cpp
@@ -0,0 +1,31 @@
+﻿//Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+//
+//Write a function which takes a list of strings and returns each line prepended by the correct number.
+//
+//The numbering starts at 1. The format is n : string.Notice the colon and space in between.
+//
+//Examples : (Input-- > Output)
+//
+//[] -- > []
+//["a", "b", "c"] -- > ["1: a", "2: b", "3: c"]
+
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+vector<string> number(const vector<string>& lines)
+{
+    vector<string> line = lines;
+    for (size_t i = 0; i != line.size(); ++i)
+        line[i] = to_string(i+1) + ": " + line[i];
+    return line;
+}
+
+int main()
+{
+    vector<string> lines = { "a","b","c" };
+    for (auto str : number(lines))
+        cout << str << " ";
+}

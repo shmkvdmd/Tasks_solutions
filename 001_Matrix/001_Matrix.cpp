dfa751d85b7bb155f2273c1b545551ce3db9470f
shmkvdmd <demidka100@mail.ru>
2023-10-04 15:39:49 +0300

    !

---
+++ 001_Matrix/001_Matrix.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <ctime>
+using namespace std;
+
+pair<size_t, size_t> MatrixArgMax(const vector<vector<int>>& matrix)
+{
+	pair<size_t, size_t> argMax = { 0,0 };
+	int max = matrix[0][0];
+	for (size_t i = 0; i != matrix.size(); ++i)
+	{
+		for (size_t j = 0; j != matrix[i].size(); ++j)
+		{
+			if (matrix[i][j] > max)
+			{
+				max = matrix[i][j];
+				argMax = { i,j };
+			}
+		}
+	}
+	return argMax;
+}
+
+vector<vector <int>> createMatrix(const int columns, const int rows, const int start, const int end)
+{
+	vector<vector<int>> matrix(rows);
+	for (size_t i = 0; i != rows; ++i)
+	{
+		matrix[i].resize(columns);
+		for (size_t j = 0; j != columns; ++j)
+			matrix[i][j] = (rand() % (end - start + 1) + start);
+	}
+	return matrix;
+}
+
+void printMatrix(vector<vector<int>> matrix)
+{
+	for (vector<int>& vec_current : matrix)
+	{
+		for (int& value : vec_current)
+			cout << value << " ";
+		cout << endl;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+	vector<vector<int>> matrix;
+	int columns, rows, start, end;
+	cout << "\tРазмер матрицы\nКол-во столбцов: "; cin >> columns;
+	cout << "Кол-во строк: "; cin >> rows;
+	cout << "\tДиапазон значений\nОт: "; cin >> start;
+	cout << "До: "; cin >> end;
+	matrix = createMatrix(columns, rows, start, end);
+	printMatrix(matrix);
+	cout << "\n\nМаксимальное значение находится в: " << MatrixArgMax(matrix).first << ", " << MatrixArgMax(matrix).second;
+}
---
+++ 002_passCheck/002_passCheck.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+//Пароли
+//Пароль от некоторого сервиса должен удовлетворять таким ограничениям :
+//
+//состоять из символов таблицы ASCII с кодами от 33 до 126;
+//быть не короче 8 символов и не длиннее 14;
+//из 4 классов символов — большие буквы, маленькие буквы, цифры, прочие символы — в пароле должны присутствовать не менее трёх любых.
+//Напишите программу, которая проверит, что введённый пароль подходит под эти ограничения.
+
+bool IsGood(string password)
+{
+	int upper = 0, lower = 0, digits = 0, other = 0;
+	for (char symbol : password)
+	{
+		if (symbol < 33 || symbol > 126)
+			return false;
+		if (symbol >= 'A' && symbol <= 'Z')
+			upper = 1;
+		else if (symbol >= 'a' && symbol <= 'z')
+			lower = 1;
+		else if (symbol >= '0' && symbol <= '9')
+			digits = 1;
+		else other = 1;
+	}
+	if (password.length() < 8 || password.length() > 14)
+		return false;
+	return upper + lower + digits + other >= 3;
+}
+int main() {
+	string pass;
+	cin >> pass;
+	if (IsGood(pass))
+		cout << "YES";
+	else cout << "NO";
+}

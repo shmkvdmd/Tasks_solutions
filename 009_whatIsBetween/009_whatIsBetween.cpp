51a583758536ce6b281f03e454b75c44af043b49
shmkvdmd <demidka100@mail.ru>
2023-10-08 11:59:30 +0300

    !

---
+++ 009_whatIsBetween/009_whatIsBetween.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+#include <vector>
+
+std::vector<int> between(int start, int end) {
+    std::vector<int> arr;
+    for (int i = start; i <= end; ++i)
+    {
+        arr.push_back(i);
+    }
+    return arr;
+}
+int main()
+{
+    for (auto i : between(1, 10))
+        std::cout << i << " ";
+}
+

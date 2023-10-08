f67e4319e5ddac4b599e2ca4b335c9790f939f9e
shmkvdmd <demidka100@mail.ru>
2023-10-08 12:12:47 +0300

    !

--- 009_whatIsBetween/009_whatIsBetween.cpp
+++ 009_whatIsBetween/009_whatIsBetween.cpp
@@ -1,4 +1,5 @@
-﻿
+﻿//Complete the function that takes two integers(a, b, where a < b) 
+//and return an array of all integers between the input parameters, including them.
 #include <iostream>
 #include <vector>
 
---
+++ 010_findMaxAndMin/010_findMaxAndMin.cpp
@@ -0,0 +1,39 @@
+﻿//Your task is to make two functions(max and min, or maximum and minimum, etc., depending on the language) 
+//that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+#include <iostream>
+#include <vector>
+#include <algorithm>
+int min(std::vector<int> list) 
+{
+    int min = list[0];
+    for (auto i : list)
+        if (i < min)
+            min = i;
+    return min;
+}
+
+int max(std::vector<int> list)
+{
+    int max = list[0];
+    for (auto i : list)
+        if (i > max)
+            max = i;
+    return max;
+}
+
+//int max(std::vector<int> list)
+//{
+//    return *(std::max_element(list.begin(), list.end()));
+//}
+//int min(std::vector<int> list)
+//{
+//    return *(std::min_element(list.begin(), list.end()));
+//}
+
+int main()
+{
+    std::vector<int> list = { 52,312,512,67,12,74,9,4 };
+    std::cout << "min = " << min(list);
+    std::cout << "\nmax = " << max(list);
+}
+

fa711f1e76956f76e0071bc55d2f21c14b637727
shmkvdmd <demidka100@mail.ru>
2023-10-10 16:31:41 +0300

    !

---
+++ 011_alternateCase/011_alternateCase.cpp
@@ -0,0 +1,16 @@
+﻿//Each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+#include <iostream>
+#include <string>
+
+std::string to_alternating_case(const std::string& str)
+{
+	std::string anotherStr = str;
+	for (char& i : anotherStr)
+		isupper(i) ? i = tolower(i) : i = toupper(i);
+	return anotherStr;
+}
+int main()
+{
+	std::cout << to_alternating_case("joPA");
+}
+

225ee183ebefe3991bf7750ce9f9e34b3342144b
shmkvdmd <demidka100@mail.ru>
2023-10-14 21:19:31 +0300

    !

--- 013_TwoSum/013_TwoSum.cpp
+++ 013_TwoSum/013_TwoSum.cpp
@@ -1,4 +1,6 @@
-﻿
+﻿//Write a function that takes an array of numbers(integers for the tests) and a target number.
+//It should find two different items in the array that, when added together, give the target value.
+//The indices of these items should then be returned in a tuple / list(depending on your language) like so : (index1, index2).
 #include <iostream>
 #include <vector>
 

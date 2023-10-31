8a8d874ac9766acb133a87c059dab8e1d97a4a6c
shmkvdmd <demidka100@mail.ru>
2023-10-31 21:50:50 +0300

    !

---
+++ 016_CountingDuplicates/016_CountingDuplicates.cpp
@@ -0,0 +1,30 @@
+﻿//Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric
+//digits that occur more than once in the input string.
+//The input string can be assumed to contain only alphabets(both uppercase and lowercase) and numeric digits.
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+size_t duplicateCount(const std::string& in)
+{
+	std::unordered_map<char, int> charCount;
+	std::string str = in;
+	for (size_t i = 0; i != str.size(); ++i)
+	{
+		str[i] = tolower(str[i]);
+		charCount[str[i]]++;
+	}
+	int count = 0;
+	for (const auto& pair : charCount)
+	{
+		if (pair.second > 1)
+			count++;
+	}
+	return count;
+}
+
+int main()
+{
+	std::string str = "Indivisibilities";
+	std::cout << duplicateCount(str);
+}

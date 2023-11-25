f4ed5834188910a382d087a448101fa85818e94a
shmkvdmd <demidka100@mail.ru>
2023-11-26 00:17:20 +0300

    !

---
+++ 018_squareMatrixMultiplication/018_squareMatrixMultiplication.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> matrix_multiplication(std::vector<std::vector<int>>& a, std::vector<std::vector<int>>& b, size_t n) {
+	std::vector<std::vector<int>> c(n, std::vector<int>(n, 0));
+	for (size_t i = 0; i < n; ++i)
+	{
+		for (size_t j = 0; j < n; ++j)
+		{
+			for (size_t k = 0; k < n; ++k)
+			{
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+	return c;
+}
+
+int main()
+{
+	std::vector<std::vector<int>> a = { {1 ,2}, {3,2} };
+	std::vector<std::vector<int>> b = { {3,2},{1,1} };
+	for (const auto& num : matrix_multiplication(a, b, 2))
+	{
+		for (const auto& n : num)
+		{
+			std::cout << n << " ";
+		}
+	}
+}
---
+++ 019_stringIncrementer/019_stringIncrementer.cpp
@@ -0,0 +1,43 @@
+﻿//Your job is to write a function which increments a string, to create a new string.
+//
+//If the string already ends with a number, the number should be incremented by 1.
+//If the string does not end with a number.the number 1 should be appended to the new string.
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+
+int main()
+{
+	std::string str = "foobar99";
+	std::string intstr;
+	std::string zero_string;
+	int temp;
+	for (size_t i = 0; i != str.length(); ++i)
+	{
+		if (isdigit(str[i]))
+		{
+			intstr = str.substr(i);
+			str.erase(i);
+			for (size_t j = 0; j != intstr.length(); ++j)
+			{
+				if (intstr[j] == '0')
+				{
+					zero_string += '0';
+				}
+			}
+			temp = std::stoi(intstr);
+			temp++;
+			intstr = std::to_string(temp);
+			str.append(zero_string);
+			str.append(intstr);
+			break;
+		}
+	}
+	if (str.empty())
+	{
+		str = '1';
+	}
+	cout << str;
+}
